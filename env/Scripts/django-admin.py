4f584085887838e12460d70abce7f457c19a8c68
fintank <finapptank@gmail.com>
2020-08-29 02:47:59 -0400

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\franc\documents\ashley website\cake_n_tingz\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
